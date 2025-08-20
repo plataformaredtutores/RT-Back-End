38912995beb537dbd860415d2e372dd70b83f030
dlv237 <dolobos@uc.cl>
2025-08-19 22:04:29 -0400

    refactor: remove dist directory and update .gitignore to exclude it

--- dist/middlewares/notFound.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = notFound;
-function notFound(_req, res, _next) {
-    res.status(404).json({ ok: false, message: 'Resource not found' });
-}
--- dist/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// routes/userRoutes.ts
-const express_1 = require("express");
-const userController_1 = require("../controllers/userController");
-const router = (0, express_1.Router)();
-router.get('/', userController_1.getUsers); // GET /users → llama a getUsers()
-exports.default = router;
